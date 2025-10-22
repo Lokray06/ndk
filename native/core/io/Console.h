19e592bf1b9dd4086daab47c74ae53f38defbe74
Lokray06 <jpgp14s06@gmail.com>
2025-10-22 10:11:10 +0200

    Add unsigned integer print and println methods to Console class; implement heap string cleanup

--- native/core/io/Console.h
+++ native/core/io/Console.h
@@ -9,27 +9,33 @@ extern "C" {
 // --- Print Methods ---
 void nebula_core_io_Console_print__Object___void(void* anything); // Object is always passed as a generic pointer
 void nebula_core_io_Console_print__bool__void(bool boolean);
-void nebula_core_io_Console_print__char__void(char character);
-void nebula_core_io_Console_print__string__void(const nebula_String_t* anyString);
-void nebula_core_io_Console_print__byte__void(int8_t number);
-void nebula_core_io_Console_print__short__void(int16_t number);
-void nebula_core_io_Console_print__int__void(int32_t number);
-void nebula_core_io_Console_print__long__void(int64_t number);
-void nebula_core_io_Console_print__float__void(float number);
-void nebula_core_io_Console_print__double__void(double number);
+void nebula_core_io_Console_print__char___void(char character);
+void nebula_core_io_Console_print__string___void(const nebula_String_t* anyString);
+void nebula_core_io_Console_print__byte___void(int8_t number);
+void nebula_core_io_Console_print__ubyte___void(uint8_t number);
+void nebula_core_io_Console_print__short___void(int16_t number);
+void nebula_core_io_Console_print__ushort___void(uint16_t number);
+void nebula_core_io_Console_print__int___void(int32_t number);
+void nebula_core_io_Console_print__uint___void(uint32_t number);
+void nebula_core_io_Console_print__long___void(int64_t number);
+void nebula_core_io_Console_print__ulong___void(uint64_t number);
+void nebula_core_io_Console_print__float___void(float number);
+void nebula_core_io_Console_print__double___void(double number);
 
 // --- Println Methods ---
-void nebula_core_io_Console_println__void();
-void nebula_core_io_Console_println__Object___void(void* anything);
-void nebula_core_io_Console_println__bool__void(bool boolean);
-void nebula_core_io_Console_println__char__void(char character);
+void nebula_core_io_Console_println__bool___void(bool boolean);
+void nebula_core_io_Console_println__char___void(char character);
 void nebula_core_io_Console_println__string___void(const nebula_String_t* anyString);
-void nebula_core_io_Console_println__byte__void(int8_t number);
-void nebula_core_io_Console_println__short__void(int16_t number);
-void nebula_core_io_Console_println__int__void(int32_t number);
-void nebula_core_io_Console_println__long__void(int64_t number);
-void nebula_core_io_Console_println__float__void(float number);
-void nebula_core_io_Console_println__double__void(double number);
+void nebula_core_io_Console_println__byte___void(int8_t number);
+void nebula_core_io_Console_println__ubyte___void(uint8_t number);
+void nebula_core_io_Console_println__short___void(int16_t number);
+void nebula_core_io_Console_println__ushort___void(uint16_t number);
+void nebula_core_io_Console_println__int___void(int32_t number);
+void nebula_core_io_Console_println__uint___void(uint32_t number);
+void nebula_core_io_Console_println__long___void(int64_t number);
+void nebula_core_io_Console_println__ulong___void(uint64_t number);
+void nebula_core_io_Console_println__float___void(float number);
+void nebula_core_io_Console_println__double___void(double number);
 
 /*
 // --- Input Methods ---
